d96e1cbd39da178566a55af788af05e25dbac2df
David-GC-5403 <davidgc42003@gmail.com>
2025-07-25 11:14:01 +0200

    Cambios que creia que ya estaban

--- bueno/Program/time_calc.py
+++ bueno/Program/time_calc.py
@@ -2,6 +2,7 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from haversine import haversine
 import csv
+from datetime import datetime, timedelta, timezone
 import time
 
 def config_influx():
@@ -11,7 +12,7 @@ def config_influx():
     url = "http://localhost:8086"
 
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org) # Cliente de InfluxDB
-    writer = client.write_api()  # API de escritura
+    writer = client.write_api(write_options=SYNCHRONOUS)  # API de escritura
     reader = client.query_api()  # API de lectura
 
     return writer, reader, org, bucket
@@ -27,9 +28,10 @@ def read_file():
 
 def read_influx(reader_api, org):
     query = 'from(bucket: "Alumnos")\
-  |> range(start: -5m)\
+  |> range(start: -15m)\
   |> filter(fn: (r) => r["_measurement"] == "mqtt_consumer")\
-  |> filter(fn: (r) => r["_field"] == "uplink_message_decoded_payload_decoded_latitud" or r["_field"] == "uplink_message_decoded_payload_decoded_longitud" or r["_field"] == "uplink_message_decoded_payload_decoded_v")'
+  |> filter(fn: (r) => r["_field"] == "uplink_message_decoded_payload_decoded_latitud" or r["_field"] == "uplink_message_decoded_payload_decoded_longitud" or r["_field"] == "uplink_message_decoded_payload_decoded_v")\
+  |> last()'
 
     result = reader_api.query(org=org, query=query)
     return result
@@ -67,30 +69,67 @@ def write_influx(writer_api, bucket, org, tiempo_restante):
 # Main #
 
 # Configuración inicial
-[writer, reader, org, bucket] = config_influx()
-stops = read_file()
-last_data = None  # Variable para almacenar los últimos datos leídos
-
 while True:
-    data_influx = read_influx(reader, org)
-    if data_influx == last_data or None:
-        print("No hay nuevos datos en Influx. Esperando...")
-        time.sleep(1*60)
+    try:
+        [writer, reader, org, bucket] = config_influx()
+        stops = read_file()
+        timestamp = None
+
+        break
+
+    except Exception as e:
+        print(f"Error al configurar InfluxDB o leer el archivo: {e}")
+        time.sleep(1)
         continue
 
-    lat_bus = data_influx[0].records[0].get_value()
-    lon_bus = data_influx[1].records[0].get_value()
-    v_bus = data_influx[2].records[0].get_value()
+# Loop principal
+while True:
+    data_influx = read_influx(reader, org)
 
-    [_, dist_parada] = parada_mas_cercana(lat_bus, lon_bus, stops)
+    # Comprueba si hay datos nuevos viendo si ha cambiado el timestamp
+    try:
+        if data_influx[0].records[0].get_time() == timestamp or data_influx[0].records[0].get_value() is None:
+            print("No hay nuevos datos en Influx. Esperando...")
+            time.sleep(30) # Pausa para dejar que lleguen los datos
+            continue
+    except IndexError:
+        print("No se han encontrado datos en Influx. Esperando...")
+        time.sleep(30)
+        continue
 
-    tiempo_restante = calc_tiempo(dist_parada, v_bus)
-    print(f"Tiempo restante para llegar a la parada más cercana: {tiempo_restante:.2f} segundos")
+    # Si hay datos nuevos, procesa la información
+    try:
+        timestamp = data_influx[0].records[0].get_time() # Hora de llegada de los datos
+        lat_bus = data_influx[0].records[0].get_value()
+        lon_bus = data_influx[1].records[0].get_value()
+        v_bus = data_influx[2].records[0].get_value()
 
-    write_influx(writer, bucket, org, tiempo_restante)
-    time.sleep(1)
-    print("Datos escritos en Influx")
+        [_, dist_parada] = parada_mas_cercana(lat_bus, lon_bus, stops)
 
-    last_data = data_influx
+        tiempo_restante = calc_tiempo(dist_parada, v_bus)
+        print(f"Tiempo restante para llegar a la parada más cercana: {tiempo_restante:.2f} segundos")
 
-    time.sleep(5*60)
+        write_influx(writer, bucket, org, 10)
+        print("Datos escritos en Influx")
+
+        last_lat = lat_bus
+        last_lon = lon_bus
+
+        cambio_brusco = haversine((lat_bus, lon_bus), (last_lat, last_lon), unit='m') > 50  # Cambia brusco si se mueve más de 50 metros
+        if cambio_brusco:
+            proxima_medida = timestamp + timedelta(minutes=2) # Espera al siguiente mensaje, a los 2 minutos, si hay cambio brusco
+        else:
+            proxima_medida = timestamp + timedelta(minutes=5) # Espera al siguiente mensaje, a los 5 minutos, si no hay cambio brusco
+
+        while datetime.now(timezone.utc) < proxima_medida: # Comprueba la hora actual. Si es menor que la hora de la proxima medida, espera
+            segundos_espera = (proxima_medida - datetime.now(timezone.utc)).total_seconds()
+            print(f"Hora actual: {datetime.now(timezone.utc).strftime('%H:%M:%S')}")
+            print(f"Esperando hasta: {proxima_medida.strftime('%H:%M:%S')} ({int(segundos_espera)} segundos)")
+
+            time.sleep(segundos_espera)
+
+    except Exception as e: 
+        print(f"Error al procesar los datos: {e}")
+        #time.sleep(60)
+        continue
+
